c98e84f159b56167d58894215ddebe5d21f497b5
Daniel Schlegel <daniel.schlegel@gmx.net>
2011-08-10 18:13:01 +0200

    added custom variables to google module

--- lib/analytical/modules/google.rb
+++ lib/analytical/modules/google.rb
@@ -8,8 +8,19 @@ module Analytical
         @tracking_command_location = :head_append
       end
 
-      def init_javascript(location)
+      # slot: 1-5 allowed
+      # key: String
+      # value: String
+      # scope: 1 (visitor-level), 2 (session-level), or 3 (page-level)
+      # custom_variables: Array of Objects [{:slot => 1, :key => "key1", :value => "value1", :scope => 3}, {:slot => 2, :key => "key2", :value => "value2", :scope => 1}]
+      def init_javascript(location, custom_variables=nil)
         init_location(location) do
+          if custom_variables
+            custom_variable_string = ""
+            custom_variables.each do |v|
+              custom_variable_string << "_gaq.push(['_setCustomVar', #{v.slot}, #{v.key}, #{v.value}, #{v.scope}]);"
+            end
+          end
           js = <<-HTML
           <!-- Analytical Init: Google -->
           <script type="text/javascript">
@@ -17,6 +28,7 @@ module Analytical
             _gaq.push(['_setAccount', '#{options[:key]}']);
             _gaq.push(['_setDomainName', '#{options[:domain]}']);
             #{"_gaq.push(['_setAllowLinker', true]);" if options[:allow_linker]}
+          #{custom_variable_string if custom_variable_string}
             _gaq.push(['_trackPageview']);
             (function() {
               var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
@@ -29,17 +41,25 @@ module Analytical
         end
       end
 
+      # slot: 1-5 allowed
+      # key: String
+      # value: String
+      # scope: 1 (visitor-level), 2 (session-level), or 3 (page-level)
+      def custom_variable(slot, key, value, scope)
+        "_gaq.push(['_setCustomVar', #{slot}, #{key}, #{value}, #{scope} ]);"
+      end
+
       def track(*args)
         "_gaq.push(['_trackPageview'#{args.empty? ? ']' : ', "' + args.first + '"]'});"
       end
-      
+
       def event(name, *args)
         data = args.first || {}
         data = data[:value] if data.is_a?(Hash)
         data_string = !data.nil? ? ", #{data}" : ""
         "_gaq.push(['_trackEvent', \"Event\", \"#{name}\"" + data_string + "]);"
       end
-      
+
     end
   end
 end
